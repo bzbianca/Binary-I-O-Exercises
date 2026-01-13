ea5a0cda6ee88b5a8a68c7148170359cadc7210b
bzbianca <bzbaccay@my.waketech.edu>
2026-01-13 09:17:20 -0500

    Created Exercise17_01.txt

--- Exercise17_01.java
+++ Exercise17_01.java
@@ -10,25 +10,30 @@ import java.io.*;
  * - Integers should be separated by a space
  * - Use text I/O (PrintWriter)
  * 
- * @author Your Name
+ * @author Bianca Baccay
  */
 public class Exercise17_01 {
     
     public static void main(String[] args) throws IOException {
         // TODO: Create a File object for "Exercise17_01.txt"
-        
-        
+        File exercise = new File("Exercise17_01.txt");
+
         // TODO: Create PrintWriter in APPEND mode
         // Hint: Use FileWriter with append parameter set to true
-        
-        
+        try (PrintWriter output = new PrintWriter(new FileWriter("Exercise17_01.txt", true))) {
+
+
         // TODO: Generate and write 100 random integers
         // Hint: Use Math.random() to generate random integers
         // Hint: Separate integers with a space
-        
+            for (int i = 0; i < 101; i++) {
+                output.print((int) (Math.random() * 101) + " ");
+            }
+
+        }
         
         // TODO: Close the file (or use try-with-resources)
-        
+
         
         System.out.println("100 random integers written to Exercise17_01.txt");
     }
